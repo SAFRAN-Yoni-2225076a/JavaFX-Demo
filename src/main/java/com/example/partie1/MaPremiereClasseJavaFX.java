9196fe3439e2408230ebf60c74be95ca2c4ce924
SAFRAN Yoni <s22002518@A-F002115.salsa.univ-amu.fr>
2023-05-03 08:53:00 +0200

    Initial commit

---
+++ src/main/java/com/example/partie1/MaPremiereClasseJavaFX.java
@@ -0,0 +1,12 @@
+package com.example.partie1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MaPremiereClasseJavaFX extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // le code pour enrichir votre fenêtre
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.tp1_javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.tp1_javafx to javafx.fxml;
+    exports com.example.tp1_javafx;
+    exports com.example.partie1;
+}
